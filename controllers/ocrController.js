8a9c24d048cf2ef8eff5f02faddaf13b1cf29910
Caio Cohen <caiocds1@gmail.com>
2024-10-17 09:25:52 -0300

    separando IDs e Voted da imagem completa

--- controllers/ocrController.js
+++ controllers/ocrController.js
@@ -1,5 +1,7 @@
 const ocrService = require('../services/ocrService');
 const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
 
 exports.processImage = async (req, res) => {
   try {
@@ -43,6 +45,100 @@ exports.checkVoted = async (req, res) => {
   } catch (error) {
     console.error('Erro ao processar a imagem:', error);
     res.status(500).json({ error: 'Erro ao processar a imagem' });
+  }
+};
+
+exports.splitImage = async (req, res) => {
+  try {
+    // Verifica se o arquivo foi enviado
+    if (!req.file) {
+      return res.status(400).json({ error: 'Nenhuma imagem enviada' });
+    }
+
+    const imagePath = path.resolve(req.file.path);
+    let outputDir = path.resolve('uploads/ids'); // Diretório de saída para as imagens divididas
+
+    // IDs
+
+    // Certifique-se de que o diretório de saída existe
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    // Parâmetros para o corte da imagem (ajuste conforme necessário)
+    let width = 2050;
+    let height = 1330;
+    let rows = 5;
+    let cols = 4;
+
+    // Remove as bordas da imagem original e prepara para divisão
+    const trimmedImagePath = path.join(outputDir, `trimmed_${req.file.originalname.split('.')[0]}.jpg`);
+    await sharp(imagePath)
+      .extract({ left: 100, top: 170, width, height })
+      .toFile(trimmedImagePath);
+
+    // Array para armazenar os caminhos das imagens geradas
+    const splitImagePathsIds = [];
+
+    // Divide a imagem em 20 partes (5 linhas x 4 colunas)
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        const left = Math.floor(col * (width / cols)) + 220 - (col * 5);
+        const top = Math.floor(row * (height / rows)) + 95 - (row * 4);
+        const partWidth = Math.floor((width / cols)/3);
+        const partHeight = Math.floor((height / rows)/10.5);
+
+        const outputPath = path.join(outputDir, `part_${row}_${col}_${req.file.originalname.split('.')[0]}.jpg`);
+
+        await sharp(trimmedImagePath)
+          .extract({ left, top, width: partWidth, height: partHeight })
+          .toFile(outputPath);
+
+          splitImagePathsIds.push(outputPath);
+      }
+    }
+
+    //VOTES
+
+    outputDir = path.resolve('uploads/votes'); 
+
+    // Certifique-se de que o diretório de saída existe
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    // Parâmetros para o corte da imagem (ajuste conforme necessário)
+    width = 2050;
+    height = 1330;
+    rows = 5;
+    cols = 4;
+
+    // Array para armazenar os caminhos das imagens geradas
+    const splitImagePathsVotes = [];
+
+    // Divide a imagem em 20 partes (5 linhas x 4 colunas)
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        const left = Math.floor(col * (width / cols)) + 130 - (col * 5);
+        const top = Math.floor(row * (height / rows)) + 115 - (row * 4);
+        const partWidth = Math.floor((width / cols)*0.70);
+        const partHeight = Math.floor((height / rows)/2);
+
+        const outputPath = path.join(outputDir, `part_${row}_${col}_${req.file.originalname.split('.')[0]}.jpg`);
+
+        await sharp(trimmedImagePath)
+          .extract({ left, top, width: partWidth, height: partHeight })
+          .toFile(outputPath);
+
+          splitImagePathsVotes.push(outputPath);
+      }
+    }
+
+    // Retorna os caminhos das imagens geradas
+    res.json({ message: 'Imagem dividida com sucesso', parts: splitImagePathsIds });
+  } catch (error) {
+    console.error('Erro ao dividir a imagem:', error);
+    res.status(500).json({ error: 'Erro ao dividir a imagem' });
   }
 };
 
