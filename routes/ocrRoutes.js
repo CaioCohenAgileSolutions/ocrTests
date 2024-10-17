70592982077e914045aba1ebdd205aedf9ec48df
Caio Cohen <caiocds1@gmail.com>
2024-10-17 15:46:11 -0300

    alteracao nome do endpoint

--- routes/ocrRoutes.js
+++ routes/ocrRoutes.js
@@ -8,6 +8,6 @@ const upload = multer({ dest: 'uploads/' }); // Pasta onde os arquivos serão ar
 // Rota POST para enviar uma imagem
 router.post('/upload', upload.single('image'), ocrController.processImage);
 router.post('/checkvoted', upload.single('image'), ocrController.checkVoted);
-router.post('/split-image', upload.single('image'), ocrController.splitImage);
+router.post('/tratarVotantes', upload.single('image'), ocrController.splitImage);
 
 module.exports = router;
